33fce61846e011d63901a45a8b2a2bc6151e771e
lalaka32 <lalaka32@mail.ru>
2018-06-13 11:38:39 +0300

    Deviding by Factories, Add tegs

---
+++ ART/Assets/[1]Source/Factories/FactorySign.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Homebrew;
+
+namespace BeeFly
+{
+    [CreateAssetMenu(fileName = "FactorySign", menuName = "Factories/FactorySign")]
+    class FactorySign: Factory
+    {
+        [SerializeField]
+        GameObject prefabOFSignMain;
+
+        [SerializeField]
+        GameObject prefabOFSignSecondary;
+
+        public List<Transform> TrafficSignsMain { get; private set; } = new List<Transform>();
+
+        public List<Transform> TrafficSignsSecondary { get; private set; } = new List<Transform>();
+
+        public Transform SpawnSignMain(Vector3 pos, Quaternion rot,Transform parent)
+        {
+            var transform = this.Populate(Pool.Entities, prefabOFSignMain, pos, rot, parent);
+            TrafficSignsMain.Add(transform);
+            return transform;
+        }
+        public Transform SpawnSignSecondary(Vector3 pos, Quaternion rot, Transform parent)
+        {
+            var transform = this.Populate(Pool.Entities, prefabOFSignSecondary, pos, rot, parent);
+            TrafficSignsSecondary.Add(transform);
+            return transform;
+        }
+    }
+}
--- ART/Assets/[1]Source/Processing/ProcessingSpawn.cs
+++ ART/Assets/[1]Source/Processing/ProcessingSpawn.cs
@@ -10,8 +10,15 @@ namespace BeeFly
 {
     class ProcessingSpawn : ProcessingBase, IMustBeWiped
     {
-        [GroupBy(Tag.Cross)]
+        //Мда чёт пока не идёт у меня мыслей
+        //Я думаю как разделать наши группы чтобы
+        //Мы могли делать 2 перекрёстка с разной конфигурацией
+
+        //Я так запарился потому что хочу чтобы можно было в настр выбрать 
+        //число перекрёстков))
+        [GroupBy(Tag.SpawnerOfCross)]
         Group CrossSpawnerSpot;
+
         [GroupBy(Tag.CarPosition)]
         Group CarsSpawnerSpots;
         [GroupBy(Tag.TLPosition)]
@@ -19,26 +26,32 @@ namespace BeeFly
         [GroupBy(Tag.SignPosition)]
         Group SignSpawnerSpots;
 
+        [GroupBy(Tag.OneLane)]
+        Group Cross;
+
         public ProcessingSpawn()
         {
             Homebrew.Timer.Add(0.1f, () => Spawn());
-
         }
         void Spawn()
         {
-            Toolbox.Get<FactoryCross>().Spawn(CrossSpawnerSpot.actors[0].transform.position, Quaternion.identity,WorldParenters.None);
-            Toolbox.Get<FactoryCross>().Cross.eulerAngles = new Vector3(-90, 0, 0);
-            for (int i = 0; i < TLSpawnerSpots.actors.Count; i++)
+            //Ставить столько перекрёствов сколько захочет плеер
+            for (int indexCross = 0; indexCross < CrossSpawnerSpot.actors.Count; indexCross++)
             {
-                Toolbox.Get<FactoryCross>().SpawnTL(TLSpawnerSpots.actors[i].transform.position,Quaternion.identity);
-            }
-            //наверн сразу заспавнить всё а потом уже разбираться 
-            //где что стоит или не стоит 
-            for (int i = 0; i < SignSpawnerSpots.actors.Count; i++)
-            {
-                Toolbox.Get<FactoryCross>().SpawnSignMain(SignSpawnerSpots.actors[i].transform.position, Quaternion.identity);
-                Toolbox.Get<FactoryCross>().SpawnSignSecondary(SignSpawnerSpots.actors[i].transform.position, Quaternion.identity);
-            }
+                Toolbox.Get<FactoryCross>().Spawn(CrossSpawnerSpot.actors[indexCross].transform.position, Quaternion.identity, WorldParenters.None);
+                Toolbox.Get<FactoryCross>().Cross.eulerAngles = new Vector3(-90, 0, 0);
+                for (int indexTL = 0; indexTL < TLSpawnerSpots.actors.Count; indexTL++)
+                {
+                    Toolbox.Get<FactoryTrafficLight>().SpawnTL(TLSpawnerSpots.actors[indexTL].transform.position, Quaternion.identity, Cross.actors[indexCross].transform);
+                }
+                //наверн сразу заспавнить всё а потом уже разбираться 
+                //где что стоит или не стоит 
+                for (int indexSign = 0; indexSign < SignSpawnerSpots.actors.Count; indexSign++)
+                {
+                    Toolbox.Get<FactorySign>().SpawnSignMain(SignSpawnerSpots.actors[indexSign].transform.position, Quaternion.identity, Cross.actors[indexCross].transform);
+                    Toolbox.Get<FactorySign>().SpawnSignSecondary(SignSpawnerSpots.actors[indexSign].transform.position, Quaternion.identity, Cross.actors[indexCross].transform);
+                }
+            }  
         }
     }
 }
